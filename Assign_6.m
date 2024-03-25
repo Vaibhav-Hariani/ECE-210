92a4a8501c2303ef3dbc67ff9615151cced483ea
Vaibhav Hariani <62775035+Vaibhav-Hariani@users.noreply.github.com>
2024-03-25 13:43:34 -0400

    Finished up Assignment 6

--- Assign_6.m
+++ Assign_6.m
@@ -27,9 +27,37 @@ S = fftshift(fft(s));
 
 figure;
 plot(fs / num_samples * (-1 / 2:1 / 2-1), mag2db(S));
-xlabel('f [HZ]');
-ylabel("Signal, DB");
-
+xlabel('frequency, (HZ)');
+ylabel("Signal, (dB)");
 
 % #3
-H = @(z) 
+numerator = [0.76 + 0.64j; 0.69 + 0.71j; 0.87 + 0.57j;];
+numerator = [numerator; conj(numerator)];
+denominator = [0.57j + 0.78j; 0.85 + 0.48j; 0.24; 0.64];
+denominator = [denominator; conj(denominator)];
+
+[num,den] = zp2tf(numerator,denominator,0.53);
+freqs_filter = [0, fs/2, 10.^3];
+H = freqz(num,den,fs);
+H_dB = mag2db(abs(H));
+H_ph = unwrap(angle(H));
+
+figure;
+zplane(numerator, denominator);
+
+figure;
+sgtitle("Response Plots");
+
+subplot(2,1,1);
+plot(freqs_filter,H_dB);
+title("Magnitude Response of Filter");
+xlim([0,freqs_filter(end)]);
+xlabel("Frequency, (HZ)");
+ylabel("Magnitude, (dB)");
+
+subplot(2,1,2);
+plot(freqs_filter, H_ph);
+title("Phase Response of Filter");
+xlim([0,freqs_filter(end)]);
+xlabel("Frequency, (HZ)");
+ylabel("Phase, (rad)");
